ad369d024621cc135ce4a4606737eac61a40338a
Burhan Noman <mburhannoman@gmail.com>
2015-11-05 11:14:10 +0500

    Changed front file. commented the concat code temporarily to make grunt work.

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,12 +64,12 @@ sass: {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  //grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'watch', 'sass']);
+  grunt.registerTask('default', [/*'concat',*/ 'uglify', 'imagemin', 'watch', 'sass']);
 
 };
